af7c079f9e82d1668b8cdf0441e345a36c5ab3c8
Connor Barker <cbarker@splunk.com>
2021-10-22 12:10:26 -0400

    Removed map styling from definition

--- packages/google-map/src/GoogleMap.jsx
+++ packages/google-map/src/GoogleMap.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
 import SplunkVisualization from "@splunk/visualizations/common/SplunkVisualization";
-// import mapStyleDark from "./map-style.json";
+import mapStyleDark from "./map-style.json";
 
 const MyMapComponent = withScriptjs(
-  withGoogleMap((props) => <GoogleMap {...props} />)
+  withGoogleMap((props) => (
+    <GoogleMap
+      defaultZoom={props.defaultZoom}
+      defaultCenter={props.defaultCenter}
+      defaultOptions={{ styles: mapStyleDark, ...props.defaultOptions }}
+    />
+  ))
 );
 
 const CustomMap = ({ options }) => {
   return (
     <MyMapComponent
-      googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyB6PoOt3MgxIT5ORCqCk6HDuRj4ChE4h2Y"
+      googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=<YOUR_KEY_HERE>"
       loadingElement={<div style={{ height: `100%` }} />}
       containerElement={<div style={{ height: `calc(100vh - 10px)` }} />}
       mapElement={<div style={{ height: `100%` }} />}
