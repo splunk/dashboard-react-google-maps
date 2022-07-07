21be79266deee4710f8c248716702d8ca83f3819
Nusair Haq <nhaq@splunk.com>
2022-07-07 18:10:32 -0400

    change dependencies location

--- packages/google-map/src/GoogleMap.jsx
+++ packages/google-map/src/GoogleMap.jsx
@@ -1,53 +1,49 @@
-import React from "react";
-import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
-import SplunkVisualization from "@splunk/visualizations/common/SplunkVisualization";
-import mapStyleDark from "./map-style.json";
-import {
-  LoadingElement,
-  ContainerElement,
-  MapElement,
-} from "./GoogleMapStyles";
+import React from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
+import SplunkVisualization from '@splunk/visualizations/common/SplunkVisualization';
+import mapStyleDark from './map-style.json';
+import { LoadingElement, ContainerElement, MapElement } from './GoogleMapStyles';
 
 const mapStyleOptions = (defaultOptions) => ({
-  styles: mapStyleDark,
-  ...defaultOptions,
+    styles: mapStyleDark,
+    ...defaultOptions,
 });
 
 const MyMapComponent = withScriptjs(
-  withGoogleMap((props) => (
-    <GoogleMap
-      defaultZoom={props.defaultZoom}
-      defaultCenter={props.defaultCenter}
-      defaultOptions={mapStyleOptions(props.defaultOptions)}
-    />
-  ))
+    withGoogleMap((props) => (
+        <GoogleMap
+            defaultZoom={props.defaultZoom}
+            defaultCenter={props.defaultCenter}
+            defaultOptions={mapStyleOptions(props.defaultOptions)}
+        />
+    ))
 );
 
 const CustomMap = ({ options }) => {
-  return (
-    <MyMapComponent
-      googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=<YOUR_KEY_HERE>"
-      loadingElement={<div style={LoadingElement} />}
-      containerElement={<div style={ContainerElement} />}
-      mapElement={<div style={MapElement} />}
-      {...options}
-    />
-  );
+    return (
+        <MyMapComponent
+            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDVYVHYYcRwAgiEBe9vgQnt83Ic1mQ1cAw"
+            loadingElement={<div style={LoadingElement} />}
+            containerElement={<div style={ContainerElement} />}
+            mapElement={<div style={MapElement} />}
+            {...options}
+        />
+    );
 };
 
 CustomMap.config = {
-  dataContract: {},
-  optionsSchema: {},
-  key: "splunk.CustomMap",
-  name: "CustomMap",
+    dataContract: {},
+    optionsSchema: {},
+    key: 'splunk.CustomMap',
+    name: 'CustomMap',
 };
 
 CustomMap.propTypes = {
-  ...SplunkVisualization.propTypes,
+    ...SplunkVisualization.propTypes,
 };
 
 CustomMap.defaultProps = {
-  ...SplunkVisualization.defaultProps,
+    ...SplunkVisualization.defaultProps,
 };
 
 export default CustomMap;
